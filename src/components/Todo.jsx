65f41fd632ec8a74966901e186d34a794f3590fb
atoenggDev <kromosom06@gmail.com>
2024-06-06 00:54:44 +0700

    finish: feature toggle todo

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { RiCloseCircleLine } from "react-icons/ri";
 import { TiEdit } from "react-icons/ti";
 
-export const Todo = ({ todos, removeTodo }) => {
+export const Todo = ({ todos, removeTodo, completeTodo }) => {
 
 
   return todos.map((item, index) => (
-    <div className="flex justify-between items-center my-2 mx-auto text-white p-3 rounded-md w-11/12 bg-gradient-to-r from-cyan-500 to-blue-500" key={index}>
+    <div className={item.isComplete ? "bg-[#d5d7da] flex justify-between items-center my-2 mx-auto line-through opacity-40 mb-8 p-3 rounded-md w-11/12": "flex justify-between items-center my-2 mx-auto text-white p-3 rounded-md w-11/12 bg-gradient-to-r from-cyan-500 to-blue-500"} key={index}>
       <div className="flex items-center gap-2" key={item.id}>
-        <input type="checkbox" className="w-4 h-4" />
+        <input type="checkbox" className="w-4 h-4" onClick={() => completeTodo(item.id)} />
         <p>{item.text}</p>
       </div>
 
