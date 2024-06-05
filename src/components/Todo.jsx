2059195172735ae5f8b06f78900e90ff035f9343
atoenggDev <kromosom06@gmail.com>
2024-06-06 00:35:34 +0700

    finish: added feature add todo

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,20 +2,20 @@ import React from "react";
 import { RiCloseCircleLine } from "react-icons/ri";
 import { TiEdit } from "react-icons/ti";
 
-export const Todo = () => {
-  return (
-    <>
-      <div className="flex justify-between items-center my-2 mx-auto text-white p-3 rounded-md w-11/12 bg-gradient-to-r from-cyan-500 to-blue-500 ">
-        <div className="flex items-center gap-2">
-          <input type="checkbox" className="w-4 h-4" />
-          <p>test</p>
-        </div>
+export const Todo = ({ todos }) => {
 
-        <div className="flex items-center text-2xl cursor-pointer">
-          <RiCloseCircleLine className="mr-1 text-white" />
-          <TiEdit className="text-white" />
-        </div>
+
+  return todos.map((item, index) => (
+    <div className="flex justify-between items-center my-2 mx-auto text-white p-3 rounded-md w-11/12 bg-gradient-to-r from-cyan-500 to-blue-500" key={index}>
+      <div className="flex items-center gap-2" key={item.id}>
+        <input type="checkbox" className="w-4 h-4" />
+        <p>{item.text}</p>
       </div>
-    </>
-  );
+
+      <div className="flex items-center text-2xl cursor-pointer">
+        <RiCloseCircleLine className="mr-1 text-white" />
+        <TiEdit className="text-white" />
+      </div>
+    </div>
+  ));
 };
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { TodoForm } from "./TodoForm";
 import { Todo } from "./Todo";
 
 export const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
+
+    const newTodos = [todo, ...todos];
+
+    setTodos(newTodos);
+    // console.log(todo, ...todos)
+  };
+
   return (
     <div>
       <h1 className="text-2xl text-black my-8 mx-0 font-bold ">
         Buat To-Do-List Hari Ini
       </h1>
-      <TodoForm/>
-      <Todo/>
+      <TodoForm onSubmit={addTodo}/>
+      <Todo todos={todos}/>
     </div>
   );
 };
